108e48ef6f64ef10211991510f30060efbce3616
Guillaume Morel <guillaume33.morel33@laposte.net>
2021-05-06 17:16:21 +0200

    Début solution affichage

--- pages/affichageCode.js
+++ pages/affichageCode.js
@@ -3,3 +3,11 @@
  * @description : Ficher assurant les fonctions liées à la récupération du code issu du programme créé via les blocs
  */
 
+// CF "init.js" dans le fichier js du projet BlocklyDuino
+
+function getCode()
+{
+    var codePeakPre = document.getElementById('content_code');
+    var generatedCode = Blockly.Arduino.workspaceToCode(Code.workspace);
+    editor.setValue(generatedCode, 1);
+}
