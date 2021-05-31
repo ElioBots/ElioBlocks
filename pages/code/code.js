ca0d60523497fae2f48b0accf9fe11d467ebc757
Le petit lapin magique BIO <guillaume33.morel33@laposte.net>
2021-05-31 16:09:11 +0100

    Changement langage OK

--- pages/code/code.js
+++ pages/code/code.js
@@ -23,3 +23,67 @@ buttonMode.addEventListener('click', () => {
         buttonMode.innerHTML = "Light"
     }
 })
+
+// Change language
+
+// On énumère les langages disponibles
+const LANGUAGE_NAME = {
+    'en': 'English',
+    'fr': 'Français'
+}
+
+var language = document.querySelector('select');
+
+function getStringParamFromUrl(name, defaultValue)
+{
+    var val = location.search.match(new RegExp('[?&]' + name + '=([^&]+)'));
+    return val ? decodeURIComponent(val[1].replace(/\+/g, '%20')) : defaultValue;
+}
+  
+function getLang() 
+{
+    var lang = getStringParamFromUrl('lang', '');
+    if (LANGUAGE_NAME[lang] === undefined) 
+    {
+      // Default to English.
+      lang = 'fr';
+    }
+    return lang;
+}
+
+language.addEventListener('change', function () 
+{
+    // Store the blocks for the duration of the reload.
+    // MSIE 11 does not support sessionStorage on file:// URLs.
+    if (window.sessionStorage) 
+    {
+        var xml = Blockly.Xml.workspaceToDom(workspace);
+        var text = Blockly.Xml.domToText(xml);
+        window.sessionStorage.loadOnceBlocks = text;
+    }
+
+    var newLang = encodeURIComponent(this.value);
+    var search = window.location.search;
+    if (search.length <= 1) 
+    {
+        search = '?lang=' + newLang;
+    } 
+    else if (search.match(/[?&]lang=[^&]*/)) 
+    {
+        search = search.replace(/([?&]lang=)[^&]*/, '$1' + newLang);
+    } else 
+    {
+        search = search.replace(/\?/, '?lang=' + newLang + '&');
+    }
+
+    window.location = window.location.protocol + '//' + window.location.host + window.location.pathname + search;
+})
+
+function setLanguage()
+{
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "../msg/js/"+getLang()+".js";
+    document.head.appendChild(script);
+    document.getElementById("changeAppLanguage").value = 'Default';
+}
